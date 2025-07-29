caa35a8f19eb46a5559a3bb558c1069529f4e7f3
Kavya Kakkar <kavyakakkar234@gmail.com>
2025-07-29 22:57:27 +0530

    nice project

---
+++ ai-Ticket-frontend/src/components/check-auth.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function  CheckAuth({children , protectedRoute}) {
+    const navigate = useNavigate()
+    const [loading , setloading] = useState(true)
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+
+        if(protectedRoute) {
+           if(!token) {
+            navigate("/login")
+           } else {
+            setLoading(false)
+           }
+        } else {
+            if(token) {
+                navigate("/")
+            } else {
+                setLoading(false)
+            }
+        }
+    } , [navigate , protectedRoute])
+    
+    if(loading) {
+        return <div>loading..</div>
+    }
+    return children
+}
+
+export default CheckAuth;
